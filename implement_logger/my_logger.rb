3b1bb3207baac767bd1b04660d0a65a2f5978743
Yusuke Yasuo <yasuo.y@dely.jp>
2019-06-20 21:40:45 +0900

    issue 5

--- implement_logger/my_logger.rb
+++ implement_logger/my_logger.rb
@@ -3,16 +3,28 @@ class MyLogger
   include Singleton
 
   attr_accessor :info, :debug, :error
+  attr_reader :history
+
+  def initialize
+    @history = []
+  end
 
   def info(msg)
+    @history << msg
     print "INFO: #{msg}"
   end
 
   def debug(msg)
+    @history << msg
     print "DEBUG: #{msg}"
   end
 
   def error(msg)
+    @history << msg
     $stderr.print "ERROR: #{msg}"
+  end
+
+  def clear_history
+    @history = []
   end
 end
